379e71b4a71980ddf8ffd17d425b7eb811fc5d39
Artyom Zaporozhets <kraysent@gmail.com>
2022-12-15 18:11:11 +0300

    added linkedin link

--- src/components/linksContainer.jsx
+++ src/components/linksContainer.jsx
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
 import ContentContainer from "./contentContainer";
 import "./linksContainer.css";
-import gh from "./images/github-dark-theme.png";
+import gh from "../images/github-dark-theme.png";
+import linkedin from "../images/linkedin-dark-theme.png";
 import openInNewTab from "../commons";
 
 class LinksContainer extends Component {
     render() {
         var links = [
-            ["GitHub", "https://github.com/Kraysent", gh]
+            ["GitHub", "https://github.com/Kraysent", gh],
+            ["LinkedIn", "https://www.linkedin.com/in/kraysent/", linkedin]
         ]
 
         var children = []
 
         for (let [service, link, pic] of links) {
             children.push(
-                <li className="list-element" onClick={function () { openInNewTab(link) }}>
-                    <img src={pic} className="icon" />{service}
+                <li className="list-element"
+                    onClick={function () { openInNewTab(link) }}
+                    key={service}>
+                    <img src={pic} className="icon" alt={service} />{service}
                 </li>
             )
         }
